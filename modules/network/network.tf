89bb0783410b4a86b3d6fbec8f4d4aabab857ea9
vilvamani.arumugam <vilvamani.arumugam@accenture.com>
2020-11-09 16:39:29 +0530

    Added output

--- modules/network/network.tf
+++ modules/network/network.tf
@@ -53,7 +53,7 @@ resource "aws_route_table" "public_rt" {
 }
 
 resource "aws_route_table_association" "public_rt_ass" {
-  subnet_id      = [aws_subnet.public_subnet_one.id]
+  subnet_id      = aws_subnet.public_subnet_one.id
   route_table_id = aws_route_table.public_rt.id
 }
 
