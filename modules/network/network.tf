dc92a5893c253c492ca864e35e10608d5d700981
vilvamani.arumugam <vilvamani.arumugam@accenture.com>
2020-11-09 15:18:48 +0530

    Added output

--- modules/network/network.tf
+++ modules/network/network.tf
@@ -1,8 +1,8 @@
 resource "aws_vpc" "vpc" {
   cidr_block = var.vpc_cidr_range
   instance_tenancy = "default"
-  enable_dns_support = "true"
-  enable_dns_hostnames = "true"
+  enable_dns_support = true
+  enable_dns_hostnames = true
   tags       = "${merge(var.tags, map("Name", "${var.environment}-vpc", "Environment", "${var.environment}"))}"
 }
 
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "vpc_id" {
   value = module.aws_network.vpc_id
 }
+
+output "subnetone_cidr" {
+  value = cidrsubnets("10.1.0.0/16", 4, 4, 8, 4)
+}
