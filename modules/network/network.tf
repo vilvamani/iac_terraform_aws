7270fac9912d45649fcae178f252139cb4e7d7ef
vilvamani.arumugam <vilvamani.arumugam@accenture.com>
2020-11-09 16:18:22 +0530

    Added output

--- modules/network/network.tf
+++ modules/network/network.tf
@@ -39,3 +39,19 @@ resource "aws_subnet" "private_subnet_two" {
 output "vpc_id" {
   value = aws_vpc.vpc.id
 }
+
+output "public_subnet_one_id" {
+  value = aws_subnet.public_subnet_one.id
+}
+
+output "public_subnet_two_id" {
+  value = aws_subnet.public_subnet_two.id
+}
+
+output "private_subnet_one_id" {
+  value = aws_subnet.private_subnet_one.id
+}
+
+output "private_subnet_two_id" {
+  value = aws_subnet.private_subnet_two.id
+}
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,19 @@
 output "vpc_id" {
   value = module.aws_network.vpc_id
 }
+
+output "public_subnet_one_id" {
+  value = module.aws_network.public_subnet_one_id
+}
+
+output "public_subnet_two_id" {
+  value = module.aws_network.public_subnet_two_id
+}
+
+output "private_subnet_one_id" {
+  value = module.aws_network.private_subnet_one_id
+}
+
+output "private_subnet_two_id" {
+  value = module.aws_network.private_subnet_two_id
+}
