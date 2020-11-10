884a074fb978d636719916a4b2c5be7febd75b41
vilvamani.arumugam <vilvamani.arumugam@accenture.com>
2020-11-10 15:50:22 +0530

    Updated security group

--- main.tf
+++ main.tf
@@ -58,7 +58,7 @@ module "aws_bastion_sg" {
   vpc_id      = module.aws_network.vpc_id
 
   ingress_cidr_blocks      = var.bastion_traffic_cidr
-  ingress_rules            = ["https-443-tcp"]
+  ingress_rules            = ["ssh-tcp"]
 }
 
 module "aws_security_group" {
