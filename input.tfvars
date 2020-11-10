3da2052829baf0b3495a794962aeec5ca326547a
vilvamani.arumugam <vilvamani.arumugam@accenture.com>
2020-11-10 10:01:53 +0530

    Added new AWS Terraform module

--- input.tfvars
+++ input.tfvars
@@ -2,6 +2,3 @@ region = "us-east-1"
 vpc_cidr_range = "10.0.0.0/16"
 private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-tags = {
-    CreatedBy  = "vilvamani.arumugam"
-}
--- variables.tf
+++ variables.tf
@@ -1,15 +1,17 @@
 variable "region" {
-  type        = "string"
-  default = "us-east-1"
+  type        = string
+  default     = us-east-1
 }
 
 variable "vpc_cidr_range" {
-  type        = "string"
-  default = "10.0.0.0/16"
+  type        = string
+  default     = 10.0.0.0/16
 }
 
 variable "private_subnets" {
+  type        = list
 }
 
 variable "public_subnets" {
+  type        = list
 }
