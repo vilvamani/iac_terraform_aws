e0616cfb14a4f5dfa7c88afba8c97cc0d0406499
vilvamani.arumugam <vilvamani.arumugam@accenture.com>
2020-11-10 09:42:24 +0530

    Added new AWS Terraform module

--- main.tf
+++ main.tf
@@ -3,10 +3,35 @@ provider "aws" {
   version = "~> 3.0"
 }
 
+data "aws_availability_zones" "zones" {}
+
 module "aws_network" {
-  source           = "./modules/network"
-  vpc_cidr_range   = var.vpc_cidr_range
-  subnet_az        = var.subnet_az
-  subnet_cidr      = cidrsubnets(var.vpc_cidr_range, 4, 4, 4, 4)
-  tags             = var.tags
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "kubernetes-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = data.aws_availability_zones.zones.names
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = false
+  enable_vpn_gateway = false
+
+  tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "Environment"                                 = "development"
+  }
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = "1"
+    "Environment"                                 = "development"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = "1"
+    "Environment"                                 = "development"
+  }
 }
--- modules/network/variables.tf
+++ modules/network/variables.tf
@@ -13,7 +13,7 @@ variable "vpc_cidr_range" {
     default = "10.0.0.0/16"
 }
 
-variable "subnet_az" {
+variable "azs" {
 }
 
 variable "subnet_cidr" {
