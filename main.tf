1dacb17388d3f6833a39aa08ecfb78da1d8a9fc8
vilvamani.arumugam <vilvamani.arumugam@accenture.com>
2020-11-10 09:44:33 +0530

    Added new AWS Terraform module

--- main.tf
+++ main.tf
@@ -6,16 +6,20 @@ provider "aws" {
 data "aws_availability_zones" "zones" {}
 
 module "aws_network" {
-  source = "terraform-aws-modules/vpc/aws"
+  source          = "terraform-aws-modules/vpc/aws"
 
-  name = "kubernetes-vpc"
-  cidr = "10.0.0.0/16"
+  name            = "kubernetes-vpc"
+  cidr            = "10.0.0.0/16"
+  
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 
   azs             = data.aws_availability_zones.zones.names
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = false
+  single_nat_gateway = false
   enable_vpn_gateway = false
 
   tags = {
