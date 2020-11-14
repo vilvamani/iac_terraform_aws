28adead30ff5fd1064653bee0c80e49ea3035f2f
vilvamani.arumugam <vilvamani.arumugam@accenture.com>
2020-11-14 17:38:04 +0530

    route 53

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,13 @@
 output "cluster_name" {
-  value = "${local.cluster_name}"
+  value = "${var.cluster_name}"
+}
+
+output "kubeadm_token" {
+  description = "Kubeadm token"
+  value       = data.template_file.kubeadm_token.rendered
+}
+
+output "k8s_master_public_ip" {
+  description = "Cluster IP address"
+  value       = aws_eip.master.public_ip
 }
