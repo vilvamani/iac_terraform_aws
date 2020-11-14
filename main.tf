4257482d157be3be5156a22d646ac2a788abff22
vilvamani.arumugam <vilvamani.arumugam@accenture.com>
2020-11-14 12:51:52 +0530

    New changes

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ resource "aws_security_group_rule" "allow_ssh_from_cidr" {
   to_port   = 22
   protocol  = "tcp"
 
-  cidr_blocks       = ["10.0.0.0/8"]]
+  cidr_blocks       = ["10.0.0.0/8"]
   security_group_id = aws_security_group.kubernetes.id
 }
 
