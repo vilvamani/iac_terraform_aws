a9703b5559581f02485d39a995aae508ed883f29
vilvamani.arumugam <vilvamani.arumugam@accenture.com>
2020-11-14 12:01:30 +0530

    New changes

--- main.tf
+++ main.tf
@@ -18,37 +18,81 @@ resource "random_string" "suffix" {
 ##############################
 ##### AWS Security Group #####
 ##############################
-
-module "aws_bastion_sg" {
-  source = "terraform-aws-modules/security-group/aws"
-
-  name        = "bastion-sg"
-  description = "bastion-sg"
-  vpc_id      = module.aws_network.vpc_id
-
-  ingress_cidr_blocks      = var.bastion_traffic_cidr
-  ingress_rules            = ["ssh-tcp"]
+data "aws_subnet" "cluster_subnet" {
+  id = var.master_subnet_id
 }
 
-module "aws_k8s_sg" {
-  source = "terraform-aws-modules/security-group/aws"
+resource "aws_security_group" "kubernetes" {
+  vpc_id = data.aws_subnet.cluster_subnet.vpc_id
+  name   = var.cluster_name
 
-  name        = "k8s-sg"
-  description = "k8s-sg"
-  vpc_id      = module.aws_network.vpc_id
-
-  ingress_cidr_blocks      = var.k8s_traffic_cidr
-  ingress_rules            = ["https-443-tcp"]
-  ingress_with_cidr_blocks = [
+  tags = merge(
     {
-      from_port   = 30000
-      to_port     = 32000
-      protocol    = "tcp"
-      description = "K8s-service ports"
-      cidr_blocks = "10.10.0.0/16"
-    }
-  ]
+      "Name"                                               = var.cluster_name
+      format("kubernetes.io/cluster/%v", var.cluster_name) = "owned"
+    },
+    var.tags,
+  )
 }
+
+# Allow outgoing connectivity
+resource "aws_security_group_rule" "allow_all_outbound_from_kubernetes" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.kubernetes.id
+}
+
+# Allow SSH connections only from specific CIDR (TODO)
+resource "aws_security_group_rule" "allow_ssh_from_cidr" {
+  count     = length(var.ssh_access_cidr)
+  type      = "ingress"
+  from_port = 22
+  to_port   = 22
+  protocol  = "tcp"
+  # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
+  # force an interpolation expression to be interpreted as a list by wrapping it
+  # in an extra set of list brackets. That form was supported for compatibilty in
+  # v0.11, but is no longer supported in Terraform v0.12.
+  #
+  # If the expression in the following list itself returns a list, remove the
+  # brackets to avoid interpretation as a list of lists. If the expression
+  # returns a single list item then leave it as-is and remove this TODO comment.
+  cidr_blocks       = [var.ssh_access_cidr[count.index]]
+  security_group_id = aws_security_group.kubernetes.id
+}
+
+# Allow the security group members to talk with each other without restrictions
+resource "aws_security_group_rule" "allow_cluster_crosstalk" {
+  type                     = "ingress"
+  from_port                = 0
+  to_port                  = 0
+  protocol                 = "-1"
+  source_security_group_id = aws_security_group.kubernetes.id
+  security_group_id        = aws_security_group.kubernetes.id
+}
+
+# Allow API connections only from specific CIDR (TODO)
+resource "aws_security_group_rule" "allow_api_from_cidr" {
+  count     = length(var.api_access_cidr)
+  type      = "ingress"
+  from_port = 6443
+  to_port   = 6443
+  protocol  = "tcp"
+  # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
+  # force an interpolation expression to be interpreted as a list by wrapping it
+  # in an extra set of list brackets. That form was supported for compatibilty in
+  # v0.11, but is no longer supported in Terraform v0.12.
+  #
+  # If the expression in the following list itself returns a list, remove the
+  # brackets to avoid interpretation as a list of lists. If the expression
+  # returns a single list item then leave it as-is and remove this TODO comment.
+  cidr_blocks       = [var.api_access_cidr[count.index]]
+  security_group_id = aws_security_group.kubernetes.id
+}
+
 
 ###################################
 ##### Generates kubeadm token #####
