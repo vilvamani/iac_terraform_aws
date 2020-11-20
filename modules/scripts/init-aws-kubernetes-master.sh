7686609704470399874b0bad54e496c74850e433
vilvamani.arumugam <vilvamani007@gmail.com>
2020-11-20 15:57:30 +0530

    Updated output

--- modules/scripts/init-aws-kubernetes-master.sh
+++ modules/scripts/init-aws-kubernetes-master.sh
@@ -179,7 +179,7 @@ do
 done
 
 # Mount EFS Storage
-mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-62ee5f97.efs.us-east-1.amazonaws.com:/ ~/efs-mount-point
+mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport $EFS_DNS:/ ~/efs-mount-point
 
 # Mount K8s persistentvolume path
 mkdir -p ~/efs-mount-point/jenkins-blue
