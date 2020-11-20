3cf2fa9000af91695efff701296cdf61b650a3aa
vilvamani.arumugam <vilvamani007@gmail.com>
2020-11-20 10:18:28 +0530

    Updated output

--- modules/aws_efs.tf
+++ modules/aws_efs.tf
@@ -11,7 +11,7 @@ module "efs" {
 
   name               = "${var.cluster_name}-efs"
   region             = var.region
-  vpc_id             = data.aws_subnet.efs_subnet.vpc_id
+  vpc_id             = data.aws_subnet.cluster_subnet.vpc_id
   subnets            = var.worker_subnet_ids
   security_groups    = [aws_security_group.kubernetes.id]
   encrypted          = true
