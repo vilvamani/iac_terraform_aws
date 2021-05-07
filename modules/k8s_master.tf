449032f905d64c1fc84fa0e458133b396cff3218
vilvamani.arumugam <vilvamani007@gmail.com>
2021-05-07 19:03:34 +0530

    added jenkins helm

--- modules/k8s_master.tf
+++ modules/k8s_master.tf
@@ -17,7 +17,7 @@ data "template_file" "init_master" {
     asg_max_nodes = var.max_worker_count
     aws_subnets   = join(" ", concat(var.worker_subnet_ids, [var.master_subnet_id]))
     efs_dns_name  = module.efs.dns_name
-    efs_id  = module.efs.efs_id
+    efs_id        = module.efs.id
   }
 }
 
@@ -57,7 +57,7 @@ resource "aws_eip" "k8s_master_eip" {
 resource "aws_instance" "k8s_master" {
   instance_type = var.master_instance_type
 
-  ami =  var.k8s_ami_id
+  ami = var.k8s_ami_id
 
   key_name = var.key_name
 
@@ -75,7 +75,7 @@ resource "aws_instance" "k8s_master" {
 
   tags = merge(
     {
-      "Name"                                                 = join("-", [var.cluster_name, "master"])
+      "Name"                                               = join("-", [var.cluster_name, "master"])
       format("kubernetes.io/cluster/%v", var.cluster_name) = "owned"
     },
     var.k8s_master_tags,
@@ -102,5 +102,5 @@ resource "aws_eip_association" "master_assoc" {
 }
 
 output "k8s_master_public_ip" {
-  value       = aws_eip.k8s_master_eip.public_ip
+  value = aws_eip.k8s_master_eip.public_ip
 }
